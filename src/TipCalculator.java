e36f6587b6fd4e51437d478d3b31470a144520c8
BT_4N2_07 <ahelik4984@bths.edu>
2022-09-21 12:02:43 -0400

    added four variables with expressions and print statements

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -8,5 +8,13 @@ public class TipCalculator {
         double tipPercent = s.nextDouble();
         System.out.print("How many people are in your group?");
         int numberOfPeople = s.nextInt();
+        double totalTipAmount = ((tipPercent/100)*total);
+        double totalIncludingTip = totalTipAmount + total;
+        double tipPerPerson = totalTipAmount/numberOfPeople;
+        double totalPerPerson = totalIncludingTip/numberOfPeople;
+        System.out.println("Your total tip amount is: " + Math.round((totalTipAmount/100.0)*100.0));
+        System.out.println("Your total bill including tip is: " + Math.round((totalIncludingTip/100.0)*100.0));
+        System.out.println("The tip per person is: " + Math.round((tipPerPerson/100.0)*100.0));
+        System.out.println("The total per person is: " + Math.round((totalPerPerson/100.0)*100.0));
     }
 }
